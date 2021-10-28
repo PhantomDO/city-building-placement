4e73e74ee5de27070b43c21f072e51c926a40937
Anne-Sophie Koch <anne-sophie.koch1@univ-lyon2.fr>
2021-10-28 11:22:19 +0200

    create type building

--- city-building-placement/Assets/Scripts/CityBuild.cs
+++ city-building-placement/Assets/Scripts/CityBuild.cs
@@ -9,37 +9,65 @@ public class CityBuild : MonoBehaviour
     public int etendue;
     public int densite;
     public Vector2 position;
+    public float partieCentreVille;
 
-    private CityZone[,] map = new CityZone[1000, 1000];
+    private CityZone[,] mapZone = new CityZone[1029, 1029]; //must be a multiple of 3
+    private typeBuilding[,] mapBuilding = new typeBuilding[1029, 1029]; //must be a multiple of 3
 
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i <= mapZone.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= mapZone.GetUpperBound(1); j++)
+            {
+                mapZone[i, j] = CityZone.NaN;
+                mapBuilding[i, j] = typeBuilding.NaN;
+            }
+        }
         createCity();
     }
 	private void OnValidate()
 	{
+		for (int i = 0; i <= mapZone.GetUpperBound(0); i++)
+		{
+			for (int j = 0; j <= mapZone.GetUpperBound(1); j++)
+			{
+                mapZone[i, j] = CityZone.NaN;
+                mapBuilding[i, j] = typeBuilding.NaN;
+            }
+		}
         createCity();
     }
 	void OnDrawGizmos()
     {
-       
-        for (int i = 0; i < 1000; i++)
-		{
-			for (int j = 0; j < 1000; j++)
-			{
-				if (map[i,j] == CityZone.CentreVille)
+
+        for (int i = 0; i <= mapZone.GetUpperBound(0); i++)
+        {
+            for (int j = 0; j <= mapZone.GetUpperBound(1); j++)
+            {
+				if (mapZone[i,j] == CityZone.CentreVille)
 				{
                     Gizmos.color = new Color(1, 0, 0, 1f);
                     Gizmos.DrawCube(new Vector3(i,0,j), new Vector3(1, 1, 1));
                 }
-                else if (map[i, j] == CityZone.Residentiel)
+                else if (mapZone[i, j] == CityZone.Residentiel)
                 {
                     Gizmos.color = new Color(0, 1, 0, 1f);
                     Gizmos.DrawCube(new Vector3(i, 0, j), new Vector3(1, 1, 1));
                 }
             }
-		}        
+		}
+        /*
+        for (int i = 1; i <= map.GetUpperBound(0); i+=3)
+        {
+            for (int j = 1; j <= map.GetUpperBound(1); j+=3)
+            {
+                Gizmos.color = new Color(0, 1, 1, 0.2f);
+                Gizmos.DrawCube(new Vector3(i, 0, j), new Vector3(3, 1, 3));
+            }
+        }
+        */
     }
 
     public enum CityZone : int
@@ -48,6 +76,14 @@ public class CityBuild : MonoBehaviour
         CentreVille = 1,
         Residentiel = 2,
         Parc = 3,
+    }
+    public enum typeBuilding : int
+    {
+        NaN = 0,
+        Maison = 1, //zone périurbaine uniquement
+        PetitImmeuble = 2, //zone périurbaine
+        Immeuble = 3, //centre ville
+        GrosImmeuble = 4 //centre ville uniquement
     }
     public void createCity()
     {
@@ -63,11 +99,33 @@ public class CityBuild : MonoBehaviour
                 if (x >= 0 && x < 1000 && y >= 0 && y < 1000){
                     if (Random.Range(0, 100) < loiNormale(i, 0)*densite)
                     {
-                        if(i<superficyRadius*0.2)
-                            map[(int)x, (int)y] = CityZone.CentreVille;
+                        if(i<superficyRadius*partieCentreVille)
+                            mapZone[(int)x, (int)y] = CityZone.CentreVille;
                         else
-                            map[(int)x, (int)y] = CityZone.Residentiel;
+                            mapZone[(int)x, (int)y] = CityZone.Residentiel;
                     }
+                }
+            }
+        }
+        //création building
+        for (int i = 0; i <= mapZone.GetUpperBound(0); i += 3)
+        {
+            for (int j = 0; j <= mapZone.GetUpperBound(1); j += 3)
+            {
+                int count = 0;
+				for (int k = 0; k < 3; k++)
+				{
+					for (int l = 0; l < 3; l++)
+					{
+                        if (mapZone[i+k, j+l] != CityZone.NaN)
+                            count++;
+					}
+				}
+                switch (count)
+                {
+                    case 0:
+                        break;
+
                 }
             }
         }
