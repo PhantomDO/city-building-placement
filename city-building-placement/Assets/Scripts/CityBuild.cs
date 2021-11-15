bdc545781e6d93f0a4a6017b65b485d2f587f428
Anne-Sophie Koch <anne-sophie.koch1@univ-lyon2.fr>
2021-11-15 12:43:34 +0100

    add river

--- city-building-placement/Assets/Scripts/CityBuild.cs
+++ city-building-placement/Assets/Scripts/CityBuild.cs
@@ -127,6 +127,7 @@ public class CityBuild : MonoBehaviour
             CreateEpicentres(ville);
         }
 
+        CreateRiver();
         CreateRoads(neighborHoodSize + 1);
         
         for (int i = 0; i < DimensionSize; i += (neighborHoodSize + 1))
@@ -162,10 +163,19 @@ public class CityBuild : MonoBehaviour
                     {
                         MapCase[index].occuped = true;
                     }
-                }
 
+                    if (MapCase[index].zone == Zone.Residentiel && Random.Range(0f, 1f) >= 0.98f)
+                    {
+                        CreateIndustrialZone(x, y);
+                    }
+                }
             }
         }
+    }
+
+    private void CreateIndustrialZone(int x, int y)
+    {
+        // TODO : faire la zone
     }
 
     private void CreateBuildings(int i, int j)
@@ -178,7 +188,7 @@ public class CityBuild : MonoBehaviour
             {
                 int index = (i + k) * DimensionSize + (j + l);
 
-                if (MapCase[index].occuped == true && MapCase[index].zone != Zone.Road)
+                if (MapCase[index].occuped == true && MapCase[index].zone != Zone.Road && MapCase[index].zone != Zone.Etang)
                 {
                     count++;
                 }
@@ -196,7 +206,7 @@ public class CityBuild : MonoBehaviour
                 //    MapCase[index].building = Building.Parc;
                 //    MapCase[index].zone = Zone.Parc;
                 //}
-                if (MapCase[index].zone == Zone.Road) continue;
+                if (MapCase[index].zone == Zone.Road || MapCase[index].zone == Zone.Etang) continue;
 
                 if (count == neighborHoodSize * neighborHoodSize)
                 {
@@ -224,7 +234,22 @@ public class CityBuild : MonoBehaviour
         }
         
     }
+    private void CreateRiver() //nbr de pts de la rivière
+    {
+        List<Vector3> ptsRiver = new List<Vector3>();
 
+        ptsRiver.Add(new Vector3(0, Random.Range(0, DimensionSize - 1), 0));
+        ptsRiver.Add(epicentres[0].position);
+        ptsRiver.Add(new Vector3(DimensionSize, Random.Range(0, DimensionSize - 1),0));
+
+        foreach (var v in bezier(ptsRiver)) {
+            int index = ((int)v.y * DimensionSize + (int)v.x);
+            if (index >= MapCase.Length) continue;
+            MapCase[index].occuped = true;
+            MapCase[index].zone = Zone.Etang;
+            MapCase[index].building = Building.Parc;
+        }
+    }
     private void CreateRoads(int spaceBetweenRoad)
     {
         // create road
@@ -271,5 +296,32 @@ public class CityBuild : MonoBehaviour
 	{
         //esperance = centre de la courbe
         return (1 / (etendue * Mathf.Sqrt(2 * Mathf.PI))) * Mathf.Exp(-0.5f * Mathf.Pow(((x - esperance) / etendue), 2));
+    }
+
+    int factorielle(int n)
+    {
+        if (n <= 1)
+            return 1;
+        return n * factorielle(n - 1);
+    }
+    float berstein(float t, int i, int n)
+    {
+        return (factorielle(n) / (factorielle(i) * factorielle(n - i))) * Mathf.Pow(t, i) * Mathf.Pow(1 - t, n - i);
+    }
+    Vector3[] bezier(List<Vector3> obj)
+    {
+        List<Vector3> pts = new List<Vector3>();
+        int n = obj.Count; //nbr de points
+
+        for (float t = 0; t <= 1.1; t += 0.01f)
+        {
+            Vector3 p = Vector3.zero;
+            for (int i = 0; i < n; i++)
+            {
+                p += berstein(t, i, n - 1) * obj[i];
+            }
+            pts.Add(p);
+        }
+        return pts.ToArray();
     }
 }
